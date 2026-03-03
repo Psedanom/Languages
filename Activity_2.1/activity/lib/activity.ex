cd4bbdbcb04c2c6edfa1d19af7ab983f505ca5a7
Psedanom <psedanom@gmail.com>
2026-03-02 22:56:22 -0600

    first two

--- Activity_2.1/activity/lib/activity.ex
+++ Activity_2.1/activity/lib/activity.ex
@@ -1,12 +1,26 @@
 defmodule Hw.Ariel1 do
-
   def fahrenheit_to_celsius(tempBefore) do
+    5 * (tempBefore - 32) / 9
+  end
 
+  def roots(a, b, c) do
+
+    (-b + :math.sqrt(b**2-(4*a*c)) )/(2*a)
 
   end
 
+  def sign(n) do
+    if n>0 do
+      1
+    end
+    if n<0 do
+      -1
 
+    else
+    0
+  end
 
+  end
 
 
 end
--- Activity_2.1/activity/test/activity_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ActivityTest do
-  use ExUnit.Case
-  doctest Activity
-
-  test "greets the world" do
-    assert Activity.hello() == :world
-  end
-end
